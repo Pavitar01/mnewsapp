7cfcec0f301ee19bb5db15d28edb01fc5df5d437
Pavi <mickeynegi924@gmail.com>
2023-03-23 10:33:28 +0530

    title added

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import React, { useEffect } from "react";
 import Navigation from "./Main/Navigation";
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
-
+import Head from "next/head";
 const index = () => {
   return (
     <div>
+      <title>Mynewsapp</title>
+      <meta name="newsapp" content="Top news app" />
       <Navigation />
     </div>
   );
